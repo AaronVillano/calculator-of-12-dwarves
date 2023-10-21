364086f094f28f2245542d8bd0d4b45c8b657685
emmanuel-joseph-22 <21.05559@g.batstate-u.edu.ph>
2023-10-21 08:31:17 +0800

    added import keyboard eme

--- app/src/main/java/com/activity/calculator/ui/components/Screens.kt
+++ app/src/main/java/com/activity/calculator/ui/components/Screens.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.activity.calculator.ui.theme.CalculatorTheme
 import androidx.compose.material3.TextField
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.DecimalFormat
