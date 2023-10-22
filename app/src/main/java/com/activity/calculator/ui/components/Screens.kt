8d9089829a830226e27b05e14a4df1aa137a6aef
chinagonzales <126368606+chinagonzales@users.noreply.github.com>
2023-10-22 17:37:00 +0800

    Update Screens.kt
    meow

--- app/src/main/java/com/activity/calculator/ui/components/Screens.kt
+++ app/src/main/java/com/activity/calculator/ui/components/Screens.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,13 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.activity.calculator.ui.theme.CalculatorTheme
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.DecimalFormat
+import kotlin.random.Random
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -111,14 +114,6 @@ public fun CalculatorScreen(modifier: Modifier = Modifier) {
     }
 }
 
-// Preview
-@Preview(showBackground = true)
-@Composable
-fun CalculatorPreview() {
-    CalculatorTheme {
-        CalculatorScreen()
-    }
-}
 
 // Function Helpers of Calculator
 @OptIn(ExperimentalMaterial3Api::class)
@@ -177,5 +172,62 @@ fun formatResult(result: String): String {
 
     } catch (e: NumberFormatException) {
         return result
+    }
+}
+
+
+@Preview
+@Composable
+fun SliderMinimalExample() {
+    var sliderPosition by remember { mutableFloatStateOf(50f) }
+    var randomValue by remember { mutableStateOf(0) }
+
+    fun reset() {
+        sliderPosition = 50f // default slider value
+        randomValue = 0 // default value of the generated number
+    }
+
+    Column(
+        modifier = Modifier.padding(horizontal = 10.dp),
+        verticalArrangement = Arrangement.SpaceBetween, // Place buttons at the bottom
+        horizontalAlignment = Alignment.CenterHorizontally // Center-align generated number
+    ) {
+        Text(text = sliderPosition.toInt().toString())
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            valueRange = 0f..100f
+        )
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "${randomValue}",
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 50.sp,
+                textAlign = TextAlign.Center // Center-align the generated number
+
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
+                horizontalArrangement= Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Button(
+                    modifier = Modifier.width(175.dp),
+                    onClick = { randomValue = Random.nextInt(0, sliderPosition.toInt() + 1) }) {
+                    Text("Generate")
+                }
+                Button(
+                    modifier = Modifier.width(175.dp),
+                    onClick = { reset() }) {
+                    Text("Reset")
+                }
+            }
+        }
     }
 }
