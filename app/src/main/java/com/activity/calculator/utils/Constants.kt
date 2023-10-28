d0816e0b9051329537924cf3101c876b090f49a8
jvnthny.espiritu <jvnthny.espiritu@gmail.com>
2023-10-28 10:25:58 +0800

    test fix navigation bar

--- app/src/main/java/com/activity/calculator/utils/Constants.kt
+++ app/src/main/java/com/activity/calculator/utils/Constants.kt
@@ -1,20 +1,21 @@
 package com.activity.calculator.utils
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
 import com.activity.calculator.models.BottomNavItem
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 
 object Constants {
+    val calculateIcon: ImageVector = painterResource(id = R.drawable.ic_custom_calculate)
+    val genIcon: ImageVector = painterResource(id = R.drawable.ic_custom_numgen)
     val BottomNavItems = listOf(
         BottomNavItem(
             label = "Calculator",
-            icon = Icons.Default.Home,
+            icon = calculateIcon,
             route = "calculator"
         ),
         BottomNavItem(
             label = "Number Generator",
-            icon = Icons.Default.Person,
+            icon = genIcon,
             route = "numgen"
         )
     )
