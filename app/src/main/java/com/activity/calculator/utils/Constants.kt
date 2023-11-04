80ecbec708460a5354db8d42366cd5abd63ce2f9
AaronVillano <126449090+AaronVillano@users.noreply.github.com>
2023-11-04 08:29:19 +0800

    Update Constants.kt

--- app/src/main/java/com/activity/calculator/utils/Constants.kt
+++ app/src/main/java/com/activity/calculator/utils/Constants.kt
@@ -1,21 +1,23 @@
 package com.activity.calculator.utils
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Calculate
+import androidx.compose.material.icons.filled.Pin
 import com.activity.calculator.models.BottomNavItem
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
+
+
 
 object Constants {
-    val calculateIcon: ImageVector = painterResource(id = R.drawable.ic_custom_calculate)
-    val genIcon: ImageVector = painterResource(id = R.drawable.ic_custom_numgen)
     val BottomNavItems = listOf(
+
         BottomNavItem(
             label = "Calculator",
-            icon = calculateIcon,
+            icon = Icons.Default.Calculate,
             route = "calculator"
         ),
         BottomNavItem(
             label = "Number Generator",
-            icon = genIcon,
+            icon = Icons.Default.Pin,
             route = "numgen"
         )
     )
